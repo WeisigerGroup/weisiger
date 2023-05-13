33020faadf41c4da41c718c5f7ef5dc3afe20b65
MMueller-CTE <mmueller@cte1926.com>
2023-05-12 21:38:14 -0400

    this has to work!

--- components/siteHeader.tsx
+++ components/siteHeader.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { CSSProperties, useState, useEffect } from 'react';
 import Image from 'next/image';
 
 const navbarStyle = (scrolled: boolean): CSSProperties => ({
