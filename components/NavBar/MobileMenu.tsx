92bcc444440b27fdea774ab069909d8a4f4e5422
MMueller-CTE <126174272+MMueller-CTE@users.noreply.github.com>
2023-05-16 10:54:43 -0400

    Adding mobile functionality

---
+++ components/NavBar/MobileMenu.tsx
@@ -0,0 +1,48 @@
+import React, { useState, CSSProperties } from 'react';
+
+type Link = {
+  text?:string;
+  link?:{href:string}
+};
+
+type MobileMenuProps = {
+  links?: Link[];
+  isOpen: boolean;
+  toggleMenu: (open: boolean) => void;
+};
+
+export default function MobileMenu({ links, isOpen, toggleMenu }: MobileMenuProps) {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const handleMenuClick = () => {
+    setShowLinks(!showLinks);
+  };
+
+  const linkListStyle: CSSProperties = {
+    maxHeight: showLinks ? '100vh' : '0',
+    overflow: 'hidden',
+    transition: 'max-height 0.5s ease-in-out',
+  };
+
+  const hamburgerStyle: CSSProperties = {
+    cursor: 'pointer',
+  };
+
+  return (
+    <div>
+      <div style={hamburgerStyle} onClick={handleMenuClick}>
+        {/* This is a simple hamburger icon. Replace it with an actual icon if needed. */}
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <ul style={linkListStyle}>
+        {links?.map((link, i) => (
+          <li key={i}>
+            {link && <a {...link}>{link.text}</a>}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ components/NavBar/NavBar.tsx
@@ -0,0 +1,98 @@
+import React, { CSSProperties, useState, useEffect } from 'react';
+import Image from 'next/image';
+import MobileMenu from './MobileMenu';
+
+type Props = {
+  img?: {url:string}
+  imgAlt?: string
+  imgLink?: {href:string}
+  links?: {text?:string; link?:{href:string}}[]
+  className?:string
+  color?:string
+}
+
+export default function Navbar({ img, imgAlt, imgLink, links, className, color }:Props) {
+  const [scrolled, setScrolled] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      setScrolled(offset > 50);
+    };
+
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const navbarStyle: CSSProperties = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    display: 'flex',
+    justifyContent: 'center',
+    padding: scrolled ? '10px 0' : '0',
+    transition: 'all 0.4s',
+    zIndex: 1000,
+    width: '100%', 
+  };
+
+  const containerStyle: CSSProperties = {
+    maxWidth: '1100px',
+    display: 'flex',
+    flexDirection: scrolled ? 'row' : 'column',
+    alignItems: 'center',
+    justifyContent: scrolled ? 'space-between' : 'center',
+    width: '100%',
+  };
+
+  const logoStyle = {
+    maxHeight: scrolled ? '120px' : '240px',
+    maxWidth: scrolled ? '120px' : '240px',
+    transition: 'all 0.4s',
+  };
+
+  const linksStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    style: 'none'
+  };
+
+  const linkListItemStyle = {
+    listStyle: 'none',
+    color: "63666a",
+    padding: '0 10px 10px 10px'
+  };
+
+  return (
+    <nav className={className} style={navbarStyle}>
+      <div style={containerStyle}>
+        {imgLink && <a {...imgLink}></a>}
+        {img && imgAlt && <Image style={logoStyle} src={img.url} alt={imgAlt} />}
+        {isMobile ? 
+          <MobileMenu links={links} isOpen={mobileMenuOpen} toggleMenu={setMobileMenuOpen} /> :
+          <div style={linksStyle as CSSProperties} color={color}>
+            {links?.map((link, i) => (
+              <li key={i} style={linkListItemStyle}>
+                {link && <a {...link}>{link.text}</a>}
+              </li>
+            ))}
+          </div>
+        }
+      </div>
+    </nav>
+  );
+}
