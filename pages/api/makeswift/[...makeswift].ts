6c45ed85ea716c1c8e768156eb2f984d0c25ebb8
MMueller-CTE <126174272+MMueller-CTE@users.noreply.github.com>
2023-05-18 08:58:23 -0400

    Adding Adobe branded fonts to the site

--- pages/api/makeswift/[...makeswift].ts
+++ pages/api/makeswift/[...makeswift].ts
@@ -1,3 +1,26 @@
 import { MakeswiftApiHandler } from '@makeswift/runtime/next'
 
-export default MakeswiftApiHandler(process.env.MAKESWIFT_SITE_API_KEY!)
+export default MakeswiftApiHandler(process.env.MAKESWIFT_SITE_API_KEY!, {
+    getFonts: () => [
+        {
+            family: 'korolev',
+            variants: [
+                {weight:'300', style: 'italic'},
+                {weight:'300', style: 'normal'},
+                {weight:'500', style: 'italic'},
+                {weight:'500', style: 'normal'},
+                {weight:'700', style: 'italic'},
+                {weight:'700', style: 'normal'}
+                ]
+        },
+        {
+            family: 'franklin-gothic-urw',
+            variants: [
+                {style: 'normal', weight:'300'},
+                {style: 'normal', weight:'400'},
+                {style: 'italic', weight:'400'}
+                ]
+        }
+    ]
+})
+
