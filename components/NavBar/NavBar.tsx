c0fd455b5872eb172537983ff17d841e788ae23e
Mark Mueller <mmueller@cte1926.com>
2023-05-19 10:30:37 -0400

    styling

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -53,6 +53,7 @@ const Navbar: React.FC<Props> = ({ img, imgAlt, imgLink, links, className, color
   const updatedContainerStyle = {
     ...containerStyle,
     top: scrolled ? '0': '20px',
+    bottom: scrolled ? '0' : '10px',
     flexDirection: (isMobile || scrolled ? 'row' : 'column') as 'row' | 'column',
     justifyContent: (isMobile || scrolled ? 'space-between' : 'center') as 'space-between' | 'center',
   };
