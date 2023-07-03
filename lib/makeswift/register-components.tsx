20653dc9e222b16f6b6f4bb0769c53bc681d237b
Mark Mueller <mmueller@cte1926.com>
2023-07-03 14:21:40 -0400

    bug fixes

--- lib/makeswift/register-components.tsx
+++ lib/makeswift/register-components.tsx
@@ -1,8 +1,10 @@
-import { Style, Link, TextInput, Color, Select, Shape, Image, List } from '@makeswift/runtime/controls'
+import { Style, Link, TextInput, Color, Select, Shape, Image, List, Number } from '@makeswift/runtime/controls'
 import { ReactRuntime } from '@makeswift/runtime/react'
 import  BrandedButton  from '../../components/Button'
 import { MakeswiftComponentType } from '@makeswift/runtime/components'
+import dynamic from "next/dynamic";
 import Navbar from '../../components/NavBar/NavBar'
+import { forwardNextDynamicRef } from "@makeswift/runtime/next";
 
 // Register your components here!
 
@@ -24,30 +26,97 @@ ReactRuntime.registerComponent(BrandedButton, {
 
 /*
 This is the component that changes out the standard navigation to a more dynamic navigation
-
-ReactRuntime.registerComponent(Navbar, {
-  type: MakeswiftComponentType.Navigation,
-  label: 'Navigation',
-  props: {
-    img: Image({format: Image.Format.WithDimensions}),
-    imgAlt: TextInput({ label: 'Image alt text', defaultValue: '' }),
-    imgLink: Link({label: 'Logo link'}),
-    links: List({
-      label: 'Navigation links',
-      type: Shape({
-          type: {
-              text: TextInput({ label: 'Text', defaultValue: 'Link', selectAll: true }),
-              link: Link({ label: 'On click' })
-          }
-      }),
-      getItemLabel(link) {
-          return link?.text || 'Link'
-      }
-  }),
-    backgroundColor: Color({ label: 'Color', defaultValue: 'white' }),
-    color: Color({ label: 'Text color', defaultValue: 'black' }),
-    className: Style({ properties: Style.All }),
-  },
-})
-
 */
+ReactRuntime.registerComponent(forwardNextDynamicRef((patch) =>
+dynamic(() =>
+  patch(import("../../components/NavBar/Navigation").then(({ Navigation }) => Navigation))
+)
+),
+{
+type: MakeswiftComponentType.Navigation,
+label: "Navigation",
+props: {
+  className: Style(),
+  navBackground: Color({
+    label: "Background color",
+    defaultValue: "#ffffff",
+  }),
+  navWidth: Number({
+    label: "Max content width",
+    defaultValue: 1200,
+    suffix: "px",
+    selectAll: true,
+  }),
+  logoImage: Image({
+    label: "Logo",
+    format: Image.Format.WithDimensions,
+  }),
+  logoWidth: Number({
+    label: "Logo width",
+    defaultValue: 160,
+    suffix: "px",
+    selectAll: true,
+  }),
+  logoAlt: TextInput({
+    label: "Logo alt text",
+    defaultValue: "Logo",
+    selectAll: true,
+  }),
+  logoLink: Link({ label: "Logo on click" }),
+  links: List({
+    label: "Main links",
+    type: Shape({
+      type: {
+        text: TextInput({
+          label: "Text",
+          defaultValue: "Link",
+          selectAll: true,
+        }),
+        link: Link({ label: "On click (disabled with subnav)" }),
+        subnavLinks: List({
+          label: "Subnav links",
+          type: Shape({
+            type: {
+              link: Link({ label: "On click" }),
+              linkText: TextInput({
+                label: "Text",
+                defaultValue: "Link",
+                selectAll: true,
+              }),
+            },
+          }),
+          getItemLabel(subnavLink) {
+            return subnavLink?.linkText || "Link";
+          },
+        }),
+      },
+    }),
+    getItemLabel(links) {
+      return links?.text || "Link";
+    },
+  }),
+  linkColor: Color({
+    label: "Link color",
+    defaultValue: "#000000",
+  }),
+  hoverColor: Color({
+    label: "Hover line color",
+    defaultValue: "#0F52BA",
+  }),
+  linkTextStyle: Style({ properties: [Style.TextStyle] }),
+  linkGap: Number({
+    label: "Link gap",
+    defaultValue: 32,
+    suffix: "px",
+    selectAll: true,
+  }),
+  ctaText: TextInput({
+    label: "CTA text",
+    defaultValue: "Contact us",
+    selectAll: true,
+  }),
+  ctaLink: Link({ label: "CTA on click" }),
+},
+}
+);
+
