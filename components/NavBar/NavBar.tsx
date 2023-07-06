defabc605fda7683d2381e461d2033685ac06b77
MMueller-CTE <126174272+MMueller-CTE@users.noreply.github.com>
2023-07-06 13:14:50 -0400

    Revert Nav

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -108,82 +108,82 @@ const Navbar: React.FC<Props> = ({ img, imgAlt, imgLink, links, className, onCli
         </a>}
         {isMobile ? 
           <MobileMenu links={links} isOpen={mobileMenuOpen} toggleMenu={setMobileMenuOpen} scrolled={scrolled} /> :
-          <NavigationMenu.List
-              className="flex items-center"
-              style={ linksStyle }
-            >
-              {links?.map((link, i) => (
-                <NavigationMenu.Item
-                  key={i}
-                  className="relative"
-                  style={ linkListItemStyle }
-                >
-                  {link.subnavLinks.length > 0 ? (
-                    <NavigationMenu.Trigger asChild>
-                      <span className="group cursor-pointer flex select-none items-center py-3 outline-none [text-transform:inherit]">
-                        {link.text}
-                        <svg
-                          viewBox="0 0 8 6"
-                          fill="none"
-                          className="linear group-data-[state=open]:-rotate-180 ml-2 h-[6px] w-2 stroke-current transition-transform duration-300"
-                        >
-                          <path
-                            d="M0 0L4 4L8 0"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          ></path>
-                        </svg>
-                      </span>
-                    </NavigationMenu.Trigger>
-                  ) : (
-                    <NavigationMenu.Link asChild>
-                      <Link
-                        href={link.links?.href ?? "#"}
-                        target={link.links?.target}
-                        className="group relative select-none py-3 leading-tight outline-none"
-                      >
-                        {link.text}
+          // <NavigationMenu.List
+          //     className="flex items-center"
+          //     style={ linksStyle }
+          //   >
+          //     {links?.map((link, i) => (
+          //       <NavigationMenu.Item
+          //         key={i}
+          //         className="relative"
+          //         style={ linkListItemStyle }
+          //       >
+          //         {link.subnavLinks.length > 0 ? (
+          //           <NavigationMenu.Trigger asChild>
+          //             <span className="group cursor-pointer flex select-none items-center py-3 outline-none [text-transform:inherit]">
+          //               {link.text}
+          //               <svg
+          //                 viewBox="0 0 8 6"
+          //                 fill="none"
+          //                 className="linear group-data-[state=open]:-rotate-180 ml-2 h-[6px] w-2 stroke-current transition-transform duration-300"
+          //               >
+          //                 <path
+          //                   d="M0 0L4 4L8 0"
+          //                   strokeWidth="2"
+          //                   strokeLinecap="round"
+          //                   strokeLinejoin="round"
+          //                 ></path>
+          //               </svg>
+          //             </span>
+          //           </NavigationMenu.Trigger>
+          //         ) : (
+          //           <NavigationMenu.Link asChild>
+          //             <Link
+          //               href={link.links?.href ?? "#"}
+          //               target={link.links?.target}
+          //               className="group relative select-none py-3 leading-tight outline-none"
+          //             >
+          //               {link.text}
 
-                        <div
-                          className="absolute bottom-1 left-0 h-0.5 w-full origin-bottom-right scale-x-0 transition-transform group-hover:origin-bottom-left group-hover:scale-x-100"
-                          style={ linkStyle}
-                        ></div>
-                      </Link>
-                    </NavigationMenu.Link>
-                  )}
+          //               <div
+          //                 className="absolute bottom-1 left-0 h-0.5 w-full origin-bottom-right scale-x-0 transition-transform group-hover:origin-bottom-left group-hover:scale-x-100"
+          //                 style={ linkStyle}
+          //               ></div>
+          //             </Link>
+          //           </NavigationMenu.Link>
+          //         )}
 
-                  {link.subnavLinks.length > 0 && (
-                    <NavigationMenu.Content
-                      className="animate-fadeInAndScale absolute top-full -left-2 min-w-[240px] origin-top rounded-md bg-white py-2 shadow-md ring-1 ring-black/10"
-                      asChild
-                    >
-                      <ul>
-                        {link.subnavLinks?.map((subnavLink, i) => (
-                          <li key={i}>
-                            <NavigationMenu.Link asChild>
-                              <Link
-                                href={subnavLink.link?.href ?? "#"}
-                                target={subnavLink.link?.target}
-                                className="block cursor-pointer rounded-sm px-4 py-2 text-gray-300 text-md outline-none transition-opacity hover:opacity-50"
-                              >
-                                {subnavLink.linkText}
-                              </Link>
-                            </NavigationMenu.Link>
-                          </li>
-                        ))}
-                      </ul>
-                    </NavigationMenu.Content>
-                  )}
-                </NavigationMenu.Item>
-              ))}
-            </NavigationMenu.List>
+          //         {link.subnavLinks.length > 0 && (
+          //           <NavigationMenu.Content
+          //             className="animate-fadeInAndScale absolute top-full -left-2 min-w-[240px] origin-top rounded-md bg-white py-2 shadow-md ring-1 ring-black/10"
+          //             asChild
+          //           >
+          //             <ul>
+          //               {link.subnavLinks?.map((subnavLink, i) => (
+          //                 <li key={i}>
+          //                   <NavigationMenu.Link asChild>
+          //                     <Link
+          //                       href={subnavLink.link?.href ?? "#"}
+          //                       target={subnavLink.link?.target}
+          //                       className="block cursor-pointer rounded-sm px-4 py-2 text-gray-300 text-md outline-none transition-opacity hover:opacity-50"
+          //                     >
+          //                       {subnavLink.linkText}
+          //                     </Link>
+          //                   </NavigationMenu.Link>
+          //                 </li>
+          //               ))}
+          //             </ul>
+          //           </NavigationMenu.Content>
+          //         )}
+          //       </NavigationMenu.Item>
+          //     ))}
+          //   </NavigationMenu.List>
           
-          /*
+         
           <ul style={linksStyle}>
             {links?.map((link, i) => (
               <li key={i} style={linkListItemStyle}>
-                {link.links && <a href={link.links.href} target={link.link.target}
+                {link.links && <a href={link.links.href} target={link.links.target}
               style={i === hoveredLink ? linksHoverStyle : linkStyle} 
               onMouseEnter={() => setHoveredLink(i)}
               onMouseLeave={() => setHoveredLink(-1)}
@@ -193,7 +193,7 @@ const Navbar: React.FC<Props> = ({ img, imgAlt, imgLink, links, className, onCli
               </li>
             ))}
           </ul>
-          */
+          
         }
       </div>
     </nav>
