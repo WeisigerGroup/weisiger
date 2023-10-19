2573479e572eb3f959e321daf6729ba68ebd6330
Mark Mueller <mmueller@cte1926.com>
2023-10-19 09:47:45 -0400

    update: Tax Exemption dependencies

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
-const withMakeswift = require('@makeswift/runtime/next/plugin')()
 
 /** @type {import('next').NextConfig} */
+const withMakeswift = require('@makeswift/runtime/next/plugin')()
+
 const nextConfig = {
   reactStrictMode: true,
 }
-
-
 module.exports = withMakeswift(nextConfig)
+
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer({})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,51 +1,76 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
-        red: "#BB452E",
-        blue: "#6BA4B8",
-        gray: {
-          300: "#BBBCBC",
-          500: "#63666A",
-          700: "#212322",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
         },
       },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
       keyframes: {
-        accordionSlideDown: {
+        "accordion-down": {
           from: { height: 0 },
           to: { height: "var(--radix-accordion-content-height)" },
         },
-        accordionSlideUp: {
+        "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
-        fadeIn: {
-          "0%": { opacity: 0 },
-          "100%": { opacity: 1 },
-        },
-        fadeOut: {
-          "0%": { opacity: 1 },
-          "100%": { opacity: 0 },
-        },
-        marqueeScroll: {
-          "0%": { transform: "translateX(0)" },
-          "100%": { transform: "translateX(-100%)" },
-        },
       },
       animation: {
-        accordionSlideDown:
-          "accordionSlideDown 400ms cubic-bezier(1, 0, 0.25, 1)",
-        accordionSlideUp: "accordionSlideUp 400ms cubic-bezier(1, 0, 0.25, 1)",
-        fadeIn: "fadeIn 200ms",
-        fadeOut: "fadeOut 200ms",
-        marqueeScroll: "marqueeScroll var(--marquee-duration) linear infinite",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
